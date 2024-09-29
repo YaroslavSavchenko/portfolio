1c0727eda7891993edae5e3b70090710bc36f65f
DESKTOP-40TT0F8\User <yarik300895@gmail.com>
2024-09-29 19:10:10 +0300

    add telegram and email icons

--- js/custom-script.js
+++ js/custom-script.js
@@ -1,17 +1,4 @@
 // Добавляем обработчик событий для переключения иконки при сворачивании/разворачивании списка языков
-$(document).ready(function () {
-    // Сворачивание / разворачивание для дропдауна с языками
-    $(document).on('show.bs.collapse', '#submenu1', function () {
-        $('#arrow-icon').removeClass('fa-caret-down').addClass('fa-caret-up');
-    });
-
-    $(document).on('hide.bs.collapse', '#submenu1', function () {
-        $('#arrow-icon').removeClass('fa-caret-up').addClass('fa-caret-down');
-    });
-});
-
-
-
 document.getElementById("lenDropdown").addEventListener("change", function () {
 
     var lenDropdown = document.getElementById("lenDropdown");
